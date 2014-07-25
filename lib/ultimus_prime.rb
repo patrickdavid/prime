a859c54cbe3cb7d8636e4dae2efa4177cd64b968
Patrick David & Andrew Akers <student@epicodus.com>
2014-07-25 16:26:41 -0700

    multiples of prime removed

--- lib/ultimus_prime.rb
+++ lib/ultimus_prime.rb
@@ -1,4 +1,9 @@
 def ultimus(num)
   prime = 2
   list = (prime..num).to_a
+  list.each do |prime|
+    list.delete_if {|prime| prime % 2 == 0 }
+  end
+  puts list
+  list
 end
--- spec/ultimus_prime_spec.rb
+++ spec/ultimus_prime_spec.rb
@@ -3,6 +3,6 @@ require('ultimus_prime')
 
 describe ('ultimus') do
   it("will do something") do
-    expect(ultimus(5)).to(eq([2,3,4,5]))
+    expect(ultimus(5)).to(eq([3,5]))
   end
 end
