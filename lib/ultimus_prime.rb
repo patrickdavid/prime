1bd8ab4072e25fee87a26bab5de01efa14af2d83
Patrick David & Andrew Akers <student@epicodus.com>
2014-07-25 16:13:17 -0700

    initial commit creates an array of numbers

---
+++ lib/ultimus_prime.rb
@@ -0,0 +1,4 @@
+def ultimus(num)
+  prime = 2
+  list = (prime..num).to_a
+end
---
+++ spec/ultimus_prime_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('ultimus_prime')
+
+describe ('ultimus') do
+  it("will do something") do
+    expect(ultimus(5)).to(eq([2,3,4,5]))
+  end
+end
