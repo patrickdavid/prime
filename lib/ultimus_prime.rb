4bf2fccb349e7f9eab0e175a1f83c2252b426caf
Brandon Madsen & Patrick David <student@epicodus.com>
2014-07-29 13:50:46 -0700

    You have prime numbers, however we need to refactor because it will error if the user puts in a number greater than 7.

--- lib/ultimus_prime.rb
+++ lib/ultimus_prime.rb
@@ -1,9 +1,34 @@
 def ultimus(num)
-  prime = 2
+  prime = 3
   list = (prime..num).to_a
-  list.each do |prime|
-    list.delete_if {|prime| prime % 2 == 0 }
+  prime_list = []
+  ultimate_prime_list = []
+  the_prime_list = []
+  the_final_list = []
+  the_final_list[0] = prime
+  the_final_list[1] = 5
+  the_final_list[2] = 7
+  list.each do |index|
+    if index % 2 != 0
+      prime_list.push(index)
+    end
   end
-  puts list
-  list
+  prime_list.each do |index|
+    if index % 3 != 0
+      ultimate_prime_list.push(index)
+    end
+  end
+  ultimate_prime_list.each do |index|
+    if index % 5 != 0
+      the_prime_list.push(index)
+    end
+  end
+  the_prime_list.each do |index|
+    if index % 7 != 0
+      the_final_list.push(index)
+    end
+  end
+  the_final_list
 end
+
+# puts ultimus(100)
--- spec/ultimus_prime_spec.rb
+++ spec/ultimus_prime_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('ultimus_prime')
 
 describe ('ultimus') do
-  it("will do something") do
-    expect(ultimus(5)).to(eq([3,5]))
+  it("will eliminate all numbers divisble by 2") do
+    expect(ultimus(7)).to(eq([3,5,7]))
+  end
+  it("will show just prime numbers") do
+    expect(ultimus(100)).to(eq([3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]))
   end
 end
